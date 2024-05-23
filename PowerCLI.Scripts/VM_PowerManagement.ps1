38455e636f19bc1e743a995f6aa52d9f3b6cf992
Bryan-LJX <116472589+Bryan-LJX@users.noreply.github.com>
2024-05-23 17:34:28 +0800

    Update and rename vm_OnOff.ps1 to VM_PowerManagement.ps1

--- PowerCLI.Scripts/VM_PowerManagement.ps1
+++ PowerCLI.Scripts/VM_PowerManagement.ps1
@@ -1,15 +1,23 @@
+# A simple script for VM Power Management in specified vCenter Server
+# Script will trigger Power On or GuestOS Shutdown operation on the selected VM, then checks for VM status peroidcally until desired state is reached, then outputs status
+# Future improvments: Include more operations like restart, suspend; ability to select multiple VMs for operation; optimize listing of VMs
+
+
 $vcserver = Read-Host -Prompt "Enter Host/vCenter FQDN or IP Address"
 
 connect-VIServer $vcserver
 
+#Lists out all VMs in vCenter Server
 get-vm | select name,powerstate | Format-Table *
+
+#User prompt to select VM and what operation to carry out
 $VMname = Read-Host -Prompt "Which VM would you like to select"
 $TurnOnOff = Read-Host -Prompt "Would you like to Power On (1) or Power Off (0)"
 $VM01 = get-vm "$VMname"
 $ipaddr01 = $VM01.guest.ipaddress[0]
 $VM01pwrstate = $VM01.powerstate
 
-
+# Simple if and while loop to carry out the power operation on user selected VM and check for VM status every 5 seconds until VM is powered on/off
 if ($TurnOnOff -eq "1") {
 
 	start-vm -VM $VM01 -RunAsync | Out-Null
@@ -23,6 +31,7 @@ while ($ipaddr01 -eq $null) {
 	$ipaddr01 = $VM01.guest.ipaddress[0]
 	start-sleep -seconds 5
 }
+# Once VM has IP address; report successful startup to user
 $VM01 = get-vm "$VMname"
 $VM01 | select name, @{N="IP Address";E={$_.Guest.IPAddress[0]}}
 "$VMname has started up!"
@@ -38,6 +47,7 @@ $VM01 | select name, @{N="IP Address";E={$_.Guest.IPAddress[0]}}
 	start-sleep -seconds 5
 }
 
+# Once VM status is powered down, report successful shutdown to user
 $VM01 = get-vm "$VMname"
 $VM01 | select name,powerstate
 "$VMname has shutdown succesfully."
